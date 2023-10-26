0e027d9324749c634b54da820e63b68e872910b1
Dikontay <student@ALEM-F3-L06-08.alem.school>
2023-10-26 11:13:29 +0600

    error message format

--- server/server.go
+++ server/server.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"net"
 	"sync"
@@ -35,10 +34,9 @@ func main() {
 			msgCh: make(chan string),
 		}
 		go handleIncomingRequest(client)
-	
+
 		// conn.Close()
 	}
-	
 }
 
 func handleIncomingRequest(client *Client) {
@@ -46,76 +44,81 @@ func handleIncomingRequest(client *Client) {
 	client.conn.Write([]byte("Please enter your name: "))
 	name := []byte{}
 	for {
-        buffer := make([]byte, 1)
-        n, err := client.conn.Read(buffer)
-        if err != nil {
-            handleErrors(err)
-            break
-        }
-        if n == 0 {
-            // No data was read; continue to read.
-            continue
-        }
+		buffer := make([]byte, 1)
+		n, err := client.conn.Read(buffer)
+		if err != nil {
+			handleErrors(err)
+			break
+		}
+		if n == 0 {
+			// No data was read; continue to read.
+			continue
+		}
 
-        if buffer[0] == '\n' {
-            // If a newline character is encountered, stop reading.
-            break
-        }
+		if buffer[0] == '\n' {
+			// If a newline character is encountered, stop reading.
+			break
+		}
 
-        name = append(name, buffer[0])
-    }
+		name = append(name, buffer[0])
+	}
 
-    client.name = string(name)
+	client.name = string(name)
 	clientMux.Lock()
 	clients[client] = true
 	clientMux.Unlock()
-	//client.conn.Write([]byte(fmt.Sprintf("%s has joined the caht", client.name)))
-	broadcast(fmt.Sprintf("%s has joined the chat", client.name))
+	// client.conn.Write([]byte(fmt.Sprintf("%s has joined the caht", client.name)))
+	broadcast(fmt.Sprintf("%s has joined the chat", client.name), &Client{})
 	go client.receiveMessages()
 
- 	for msg := range client.msgCh {
-        _, err := client.conn.Write([]byte(msg))
-        if err != nil {
-            break
-        }
-    }
+	for msg := range client.msgCh {
+		_, err := client.conn.Write([]byte(msg))
+		if err != nil {
+			break
+		}
+	}
 	clientMux.Lock()
-    delete(clients, client)
-    clientMux.Unlock()
-	broadcast(fmt.Sprintf("%s has left the chat", client.name))
-	
-
+	delete(clients, client)
+	clientMux.Unlock()
+	broadcast(fmt.Sprintf("%s has left the chat", client.name), &Client{})
 }
 
 func (client *Client) receiveMessages() {
-	scanner := bufio.NewScanner(client.conn)
-	
-		var msg string
-		
-		for scanner.Scan(){
-			times := time.Now().Format("2006-01-02 15:04:05")
-			msg = scanner.Text()
-			broadcast(fmt.Sprintf("[%s][%s]:%s\n", times, client.name, msg )) 
+	// scanner := bufio.NewScanner(client.conn)
+
+
+
+	for {
+		timestap := time.Now().Format("02-Jan-06 15:04:05 MST")
+		// fmt.Println(buffer)
+		client.conn.Write([]byte(fmt.Sprintf("[%s][%s]: ", string(client.name), timestap)))
+		buffer := make([]byte, 1024)
+		_, err := client.conn.Read(buffer)
+		if err != nil {
+			broadcast(fmt.Sprintf("%s has left the chat", client.name), &Client{})
+			return
 		}
 		
-		
-		
+		broadcast(fmt.Sprintf("[%s][%s]:%s\n", client.name, timestap,  string(buffer)), client)
+	}
 
 	
-	close(client.msgCh)
 }
 
-func broadcast(message string) {
+func broadcast(message string, currentClient *Client) {
 	clientMux.Lock()
 	defer clientMux.Unlock()
 	for client := range clients {
-		select {
-        case client.msgCh <- message:
-			//fmt.Println("send messages")
-        default:
-           // fmt.Printf("Failed to send message to client %s\n", client.name)
-        }
+		if client != currentClient {
+			select {
+			case client.msgCh <- message:
+				// fmt.Println("send messages")
+			default:
+				// fmt.Printf("Failed to send message to client %s\n", client.name)
+			}
+		}
 	}
+	//close(currentClient.msgCh)
 }
 
 func handleErrors(err error) {
